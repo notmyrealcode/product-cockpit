6a53cf690949511b74de14cd5501289fafa9f296
Justin Eckhouse <justin@supergood.uno>
2025-12-11 16:15:47 -0800

    Scaffold VS Code extension

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Product Cockpit is now active');
+}
+
+export function deactivate() {}
